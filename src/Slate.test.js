77419b537e807ccb8dbc3d5049e05c8f1a2b523a
Dan Selman <danscode@selman.org>
2019-08-22 11:30:16 +0200

    (wip) convert slate dom to commonmark ast
    Signed-off-by: Dan Selman <danscode@selman.org>

---
+++ src/Slate.test.js
@@ -0,0 +1,71 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// @ts-nocheck
+/* eslint-disable no-undef */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Value = require('slate').Value;
+const CommonMarkParser = require('@accordproject/markdown-transform').CommonMarkParser;
+const commonMarkToString = require('@accordproject/markdown-transform').commonMarkToString;
+const slateToCommonMarkAst = require('./slateToCommonMarkAst');
+let parser = null;
+
+// @ts-ignore
+beforeAll(() => {
+    parser = new CommonMarkParser();
+    // const result = parser.parse('this is `some code`.');
+    // const json = parser.getSerializer().toJSON(result);
+    // console.log(JSON.stringify(json, null, 4));
+    // console.log(commonMarkToString(result));
+});
+
+/**
+ * Get the name and contents of all slate test files
+ * @returns {*} an array of name/contents tuples
+ */
+function getSlateFiles() {
+    const result = [];
+    const files = fs.readdirSync(__dirname + '/../test/');
+
+    files.forEach(function(file) {
+        if(file.endsWith('.json')) {
+            let contents = fs.readFileSync(__dirname + '/../test/' + file, 'utf8');
+            result.push([file, contents]);
+        }
+    });
+
+    return result;
+}
+
+describe('slate', () => {
+    getSlateFiles().forEach( ([file, jsonText]) => {
+        it(`converts ${file} to concerto`, () => {
+            const value = Value.fromJSON(JSON.parse(jsonText));
+            const concertoObject = slateToCommonMarkAst(value.document);
+            const json = parser.getSerializer().toJSON(concertoObject);
+            console.log(JSON.stringify(json, null, 4));
+            expect(json).toMatchSnapshot();
+
+            // load expected markdown
+            const extension = path.extname(file);
+            const mdFile = path.basename(file,extension);
+            const expectedMarkdown = fs.readFileSync(__dirname + '/../test/' + mdFile + '.md', 'utf8');
+            const md = commonMarkToString(concertoObject);
+            expect(md).toEqual(expectedMarkdown);
+        });
+    });
+});
---
+++ src/slateToCommonMarkAst.js
@@ -0,0 +1,169 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const NS = 'org.accordproject.commonmark';
+
+const CommonMarkParser = require('@accordproject/markdown-transform').CommonMarkParser;
+
+/**
+ * Converts a Slate document node to CommonMark AST
+ * @param {*} document the Slate document node
+ * @returns {*} the common mark AST
+ */
+function slateToCommonMarkAst(document) {
+
+    const result = {
+        $class : 'org.accordproject.commonmark.Document',
+        xmlns : 'slate',
+        nodes : []
+    };
+    _recursive(result, document.nodes);
+    const parser = new CommonMarkParser();
+    return parser.convertToConcertoObject(result);
+}
+
+/**
+ * Converts an array of Slate nodes, pushing them into the parent
+ * @param {*} parent the parent commonmark ast node
+ * @param {*} nodes an array of Slate nodes
+ */
+function _recursive(parent, nodes) {
+
+    nodes.forEach((node, index) => {
+
+        let result = null;
+
+        switch (node.object) {
+        case 'text':
+            result = handleText(node);
+            break;
+        default:
+            switch(node.type) {
+            case 'paragraph':
+                result = {$class : `${NS}.Paragraph`, nodes: []};
+                break;
+            case 'quote':
+                result = {$class : `${NS}.BlockQuote`};
+                break;
+            case 'horizontal_rule':
+                result = {$class : `${NS}.ThematicBreak`};
+                break;
+            case 'heading_one':
+                result = {$class : `${NS}.Heading`, level : '1', text: node.nodes[0].text};
+                break;
+            case 'heading_two':
+                result = {$class : `${NS}.Heading`, level : '2', text: node.nodes[0].text};
+                break;
+            case 'heading_three':
+                result = {$class : `${NS}.Heading`, level : '3', text: node.nodes[0].text};
+                break;
+            case 'heading_four':
+                result = {$class : `${NS}.Heading`, level : '4', text: node.nodes[0].text};
+                break;
+            case 'heading_five':
+                result = {$class : `${NS}.Heading`, level : '5', text: node.nodes[0].text};
+                break;
+            case 'heading_six':
+                result = {$class : `${NS}.Heading`, level : '6', text: node.nodes[0].text};
+                break;
+            case 'block_quote':
+                result = {$class : `${NS}.BlockQuote`};
+                break;
+            case 'code_block':
+                result = {$class : `${NS}.CodeBlock`};
+                break;
+            case 'html_block':
+                result = {$class : `${NS}.HtmlBlock`};
+                break;
+            case 'html_inline':
+                result = {$class : `${NS}.HtmlInline`};
+                break;
+            }
+        }
+
+        if(!result) {
+            throw Error(`Failed to process node ${JSON.stringify(node)}`);
+        }
+
+        // process any children
+        if(node.nodes) {
+            _recursive(result, node.nodes);
+        }
+
+        if(!parent.nodes) {
+            throw new Error(`Parent node doesn't have children ${JSON.stringify(parent)}`);
+        }
+        parent.nodes.push(result);
+    });
+}
+
+/**
+ * Handles a text node
+ * @param {*} node the slate text node
+ * @returns {*} the ast node
+ */
+function handleText(node) {
+
+    let strong = null;
+    let emph = null;
+    let result = null;
+
+    const isBold = node.marks.some(mark => mark.type === 'bold');
+    const isItalic = node.marks.some(mark => mark.type === 'italic');
+    const isCode = node.marks.some(mark => mark.type === 'code');
+
+    if (isCode) {
+        return {$class : `${NS}.Code`, text: node.text};
+    }
+
+    const text = {
+        $class : `${NS}.Text`,
+        text : node.text
+    };
+
+    if (isBold) {
+        strong = {$class : `${NS}.Strong`, nodes: []};
+    }
+
+    if (isItalic) {
+        emph  = {$class : `${NS}.Emph`, nodes: []};
+    }
+
+    if(strong && emph) {
+        result = emph;
+        emph.nodes.push(strong);
+        strong.nodes.push(text);
+    }
+    else {
+        if(strong) {
+            result = strong;
+            strong.nodes.push(text);
+        }
+
+        if(emph) {
+            result = emph;
+            emph.nodes.push(text);
+        }
+    }
+
+    if(!result) {
+        result = text;
+    }
+
+    return result;
+}
+
+module.exports = slateToCommonMarkAst;
